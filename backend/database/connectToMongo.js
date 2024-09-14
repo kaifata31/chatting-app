14f45890b8da96879916008cd8170c2d7a12ccf0
Kaif <kaifata31@gmail.com>
2024-09-15 00:45:57 +0530

    resolving

--- backend/database/connectToMongo.js
+++ backend/database/connectToMongo.js
@@ -1,11 +1,13 @@
-
 import mongoose from "mongoose";
 
-export const connectMongodb=async(req,res)=>{
-   try {
-    await mongoose.connect(process.env.MONGO_URI);
+export const connectMongodb = async (req, res) => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Database connected Successfully");
-   } catch (error) {
-     console.log("Error in connecting mongodb database");
-   }
-}
+  } catch (error) {
+    console.log("Error in connecting mongodb database");
+  }
+};
