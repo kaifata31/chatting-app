57624f47d42161590e180267f80ba569e3dc9559
Kaif <kaifata31@gmail.com>
2024-09-14 23:35:13 +0530

    last commit

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -6,12 +7,11 @@ import authRouter from "./routes/auth.routes.js";
 import messageRouter from "./routes/message.routes.js";
 import userRouter from "./routes/user.routes.js";
 import { connectMongodb } from "./database/connectToMongo.js";
-import { app,server } from "./socket/socket.js";
-
-
+import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 const port = process.env.PORT;
+const __dirname = path.resolve();
 
 //middlewares
 app.use(express.json());
@@ -19,9 +19,10 @@ app.use(cookieParser());
 app.use("/api/auth", authRouter);
 app.use("/api/message", messageRouter);
 app.use("/api/users", userRouter);
+app.use(express.static(path.join(__dirname, "frontend/dist")));
 
-app.get("/", (req, res) => {
-  res.send("Hi Iam working");
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 server.listen(port, () => {
